b0969f701f20f0c7ef9abc8305658197ea294d9d
unknown <priyanshusolon2001@gmail.com>
2025-10-21 19:16:35 +0530

    user registration

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -25,11 +25,11 @@ const userSchema = new Schema({
         index:true
     },
     avatar:{
-        type:string, //cloudinary
+        type:String, //cloudinary
         required:true
     },
     coverImage:{
-        type:string
+        type:String
     },
     watchHistory:[
         {
@@ -53,7 +53,7 @@ const userSchema = new Schema({
 userSchema.pre("save",async function (next) {
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password,10)
+    this.password = await bcrypt.hash(this.password,10)
     next()
 })
 
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,4 +1,4 @@
-class APiResponse {
+class ApiResponse {
     constructor(statusCode,data,message = "Success"){
         this.statusCode = statusCode
         this.data = data
@@ -6,3 +6,5 @@ class APiResponse {
         this.success = statusCode<400
     }
 }
+
+export {ApiResponse}
